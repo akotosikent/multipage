763fccaad2feebe67574f6acd628c528ab4e20ff
akotosikent <mkbiguiz@tip.edu.ph>
2025-04-01 19:50:39 +0800

    Added dynamic blog route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,6 +19,17 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+// New Route: Serve blog posts dynamically
+app.get('/blog', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data', 'posts.json'), 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ error: 'Failed to load posts' });
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 
 // Start the server
